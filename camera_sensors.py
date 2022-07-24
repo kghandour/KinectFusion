Fixed depth, Nk, Vk, Masking

camera_sensors.py
@@ -5,7 +5,9 @@ class CamDetails():
     colorHeight= 480
     depthWidth = 640
     depthHeight= 480
-    colorIntrinsics = np.array([525.0, 0.0, 319.5, 0.0, 525.0, 239.5,0.0, 0.0, 1]).reshape((3,3))
+    colorIntrinsics = np.array([525.0, 0.0, 319.5,
+                                0.0, 525.0, 239.5,
+                                0.0, 0.0, 1]).reshape((3,3))
     depthIntrinsics = colorIntrinsics
     fX = depthIntrinsics[0, 0]
     fY = depthIntrinsics[1, 1]

icp.py
@@ -31,7 +31,6 @@ class ICPOptimizer():
         return (source_normals.dot(rotation_inverse))+translation
 
     def prune_correspondences(self, source_normals, target_normals, distances):
-        print(np.max(distances))
         matches = distances <= self.max_distance
         v1_u = normalize(source_normals, norm="l2", axis=1)
         v2_u = normalize(target_normals, norm="l2", axis=1)
@@ -63,16 +62,21 @@ class ICPOptimizer():
 
     def estimate_pose(self, source_points, target_points, source_noramls, target_normals, initial_pose=np.eye(4), show_verbose=False):
         source_points_orig = source_points.reshape(-1,3)
-        target_points_orig = source_points.reshape(-1,3)
+        target_points_orig = target_points.reshape(-1,3)
         source_noramls = source_noramls.reshape(-1,3)
         target_normals = target_normals.reshape(-1,3)
+
+
 
 
         source_points_hom = np.c_[source_points_orig, np.ones(source_points_orig.shape[0])]
         target_points_hom = np.c_[target_points_orig, np.ones(target_points_orig.shape[0])]
 
-        source_points = self.randomSample(source_points_hom, sample_rate=1)
-        target_points = self.randomSample(target_points_hom, sample_rate=1)
+        # source_points = self.randomSample(source_points_hom, sample_rate=0.2)
+        # target_points = self.randomSample(target_points_hom, sample_rate=0.2)
+
+        source_points = source_points_hom
+        target_points = target_points_hom
 
 
         tree = KDTree(target_points[:, :3], metric="euclidean")
@@ -115,7 +119,7 @@ class ICPOptimizer():
             taregt_points_used = arranged_targets[matches]
             target_normals_used = arranged_target_normals[matches]
 
-            print(matches, source_points_used, taregt_points_used, target_normals_used)
+            # print(matches, source_points_used, taregt_points_used, target_normals_used)
 
             res_lsq = least_squares(self.point_to_plane_distance, np.zeros(6),
                                     args=(source_points_used, taregt_points_used, target_normals_used), method='lm', verbose=0)

layer.py
@@ -1,7 +1,7 @@
 import cv2
 from cv2 import destroyWindow
 import numpy as np
-from PIL import ImageOps
+from PIL import ImageOps, Image
 from camera_sensors import CamDetails
 
 from transforms import Transforms
@@ -11,9 +11,9 @@ class Layer():
     def __init__(self,  depthImage, rgbImage, sensor):
         self.sensor = sensor
         self.depthImage = depthImage
-        self.rgbImage = rgbImage
-        self.dHeight = self.depthImage.shape[0]
-        self.dWidth = self.depthImage.shape[1]
+        self.rgbImage = np.swapaxes(rgbImage, 0,1)
+        self.dHeight = self.depthImage.size[1]
+        self.dWidth = self.depthImage.size[0]
 
 
         depthIntrinsics =  self.sensor.m_depthIntrinsics
@@ -24,9 +24,9 @@ class Layer():
 
 
         self.Dk = []
-        self.Vk = np.zeros((self.dHeight,  self.dWidth, 3))
-        self.Nk = np.zeros(( self.dHeight,  self.dWidth, 3))
-        self.M = np.zeros((self.dHeight, self.dWidth))
+        self.Vk = np.zeros((self.dWidth,  self.dHeight, 3))
+        self.Nk = np.zeros(( self.dWidth,  self.dHeight, 3))
+        self.M = np.zeros((self.dWidth, self.dHeight))
 
         self.compNormalsVerticesAndMask()
 
@@ -36,16 +36,14 @@ class Layer():
 
         ## Dk  --------
         ##TODO: Make sure if Depth must be divided by 5000 or not. I normalized the depth directly to 255 instead of dividing by 5000
-        imgConv = ImageOps.grayscale(self.sensor.dImageRaw)
+        normalize = np.array(self.depthImage)/72
+        image = Image.fromarray(normalize)
+        imgConv = ImageOps.grayscale(image)
         Dk = cv2.bilateralFilter(np.asarray(imgConv), 15,20,20)
-        Dk = np.asarray(Dk)
-
-        dHeight = self.dHeight
-        dWidth = self.dWidth
-
+        self.Dk = np.transpose(np.asarray(Dk))
         # Vk = Camera space
-        
-        self.Vk = Transforms.screen2cam(Dk)
+
+        self.Vk = Transforms.screen2cam(self.Dk)
         # X_range = range(self.dWidth)
         # Y_range = range(self.dHeight)
         # X, Y = np.meshgrid(X_range, Y_range)
@@ -64,15 +62,16 @@ class Layer():
 
         # Nk
         # M
-        for u in range( dHeight -1): #Neighbouring
-            for v in range( dWidth -1 ): #Neighbouring
+        for u in range( self.dWidth -1): #Neighbouring
+            for v in range( self.dHeight -1 ): #Neighbouring
                 n = np.zeros(3, )
-                if 0 <= self.Vk[u, v, 2] <= 200:
-                    n = np.cross( (self.Vk[u+1, v, :] - self.Vk[u,v,:]), self.Vk[u, v+1,:] - self.Vk[u,v,:])
-                
-                if(np.linalg.norm(n)!=0 or self.depthImage[u,v]!=0):
+                if 1 <= self.Vk[u, v, 2] <= 240:
+                    n = np.cross((self.Vk[u+1, v,:] - self.Vk[u,v,:]), self.Vk[u, v+1,:] - self.Vk[u,v,:])
+                if(np.linalg.norm(n)!=0):
                     self.Nk[u, v, :] = n/np.linalg.norm(n)
                     self.M[u,v] = 1
+
         ## Supposed to be HxW,3 , H,W,3 , H,W
-        # self.Vk = self.Vk[self.M == 1]
-        # self.Nk = self.Nk[self.M == 1]
+        self.Vk = self.Vk[self.M == 1].reshape(-1,3)
+        self.Nk = self.Nk[self.M == 1].reshape(-1,3)
+        self.rgbImage = self.rgbImage[self.M == 1].reshape(-1,3)

parser.py
@@ -11,6 +11,7 @@ from raycast import Raycast
 from layer import Layer
 from transforms import Transforms
 from tsdf import TSDFVolume
+import copy
 
 
 class Parser():
@@ -78,54 +79,76 @@ class Parser():
         return np.array(vertices_position), np.array(vertices_color)
 
     def process(self):
-        vis_pcd = o3d.visualization.Visualizer()
-        vis_pcd.create_window()
-        vis_volume = o3d.visualization.Visualizer()
-        vis_volume.create_window()
+        # vis_pcd = o3d.visualization.Visualizer()
+        # vis_pcd.create_window()
+        # vis_volume = o3d.visualization.Visualizer()
+        # vis_volume.create_window()
 
         i = 0
         pcd = o3d.geometry.PointCloud()
         while( self.sensor.processNextFrame()):
-            depthImageRaw =  self.sensor.dImage
+            depthImageRaw =  self.sensor.dImageRaw
             colorImageRaw =  self.sensor.rgbImage
-            h, w = depthImageRaw.shape
+            w,h = depthImageRaw.size
             pyramid = {}
             pyramid['l1'] = Layer(depthImageRaw, colorImageRaw, self.sensor)
-            pyramid['l2'] = Layer(cv2.resize(depthImageRaw, (int(w/2), int(h/2))), cv2.resize(colorImageRaw, (int(w/2), int(h/2))), self.sensor)
-            pyramid['l3'] = Layer(cv2.resize(depthImageRaw, (int(w/4), int(h/4))), cv2.resize(colorImageRaw, (int(w/4), int(h/4))), self.sensor)
-            self.pyramids_so_far.append(pyramid)
+            pyramid['l2'] = Layer(depthImageRaw.resize((int(w/2), int(h/2))), cv2.resize(colorImageRaw, (int(w/2), int(h/2))), self.sensor)
+            pyramid['l3'] = Layer(depthImageRaw.resize((int(w/4), int(h/4))), cv2.resize(colorImageRaw, (int(w/4), int(h/4))), self.sensor)
+            
+
             if(i==0):
-                curr_volume = self.tsdfVolume.integrate(pyramid["l1"].depthImage, pyramid["l1"].rgbImage,self.T_matrix, None)
+                pass
+                # curr_volume = self.tsdfVolume.integrate(pyramid["l1"].depthImage, pyramid["l1"].rgbImage,self.T_matrix, np.eye(4))
             else:
                 self.T_matrix = self.icp_optimizer.estimate_pose(pyramid["l1"].Vk,self.pyramids_so_far[-1]["l1"].Vk,pyramid["l1"].Nk,self.pyramids_so_far[-1]["l1"].Nk)
-                curr_volume = self.tsdfVolume.integrate(pyramid["l1"].depthImage, pyramid["l1"].rgbImage,self.T_matrix, self.prev_volume)    
-                self.Transformation_list.append(self.T_matrix)
-            
-            tsdf_volume_mesh = self.tsdfVolume.visualize()
-            self.prev_volume = curr_volume
+                # curr_volume = self.tsdfVolume.integrate(pyramid["l1"].depthImage, pyramid["l1"].rgbImage,self.T_matrix, self.prev_volume)    
+                # self.Transformation_list.append(self.T_matrix)
+                # pcd.points = o3d.utility.Vector3dVector(pyramid['l1'].Vk)
+                # pcd.colors = o3d.utility.Vector3dVector(np.swapaxes(pyramid['l1'].rgbImage,0,1).reshape(-1,3))
+                print("ICP",self.T_matrix)
+                print("Ground", self.sensor.currentTrajectory)
+
+                print("ICP Inv",np.linalg.inv(self.T_matrix))
+                print("Ground Inv", np.linalg.inv(self.sensor.currentTrajectory))
+                # pcd2 = o3d.geometry.PointCloud()
+                # pcd2.points = o3d.utility.Vector3dVector(self.pyramids_so_far[-1]['l1'].Vk)
+                # pcd2.colors = o3d.utility.Vector3dVector(np.swapaxes(self.pyramids_so_far[-1]['l1'].rgbImage,0,1).reshape(-1,3))
+                # mesh_t = copy.deepcopy(pcd).transform(self.T_matrix)
+                # print("Copied")
+
+                # o3d.visualization.draw_geometries([mesh_t, pcd2])
+                # print("Reached Vis")
+
+            self.pyramids_so_far.append(pyramid)
+            # print("Reached appending")
+
+            # tsdf_volume_mesh = self.tsdfVolume.visualize()
+            # self.prev_volume = curr_volume
 
             
             # exit()
-            st = time.time()
-            vert_pos, vert_col = self.one_loop()
-            et = time.time()
-            print("Time taken to process a frame ", et - st)
-            pcd.points = o3d.utility.Vector3dVector(vert_pos)
-            pcd.colors = o3d.utility.Vector3dVector(vert_col/255)
-            if(i == 0):
-                vis_pcd.add_geometry(pcd)
-                vis_volume.add_geometry(tsdf_volume_mesh)
-            else:
-                vis_pcd.update_geometry(pcd)
-                vis_volume.update_geometry(tsdf_volume_mesh)
+            # # st = time.time()
+            # # vert_pos, vert_col = self.one_loop()
+            # # et = time.time()
+            # # print("Time taken to process a frame ", et - st)
+            # pcd.points = o3d.utility.Vector3dVector(vert_pos)
+            # pcd.colors = o3d.utility.Vector3dVector(vert_col/255)
+            # if(i == 0):
+            #     vis_pcd.add_geometry(pcd)
+            #     # vis_volume.add_geometry(tsdf_volume_mesh)
+            # else:
+            #     vis_pcd.update_geometry(pcd)
+            #     # vis_volume.update_geometry(tsdf_volume_mesh)
 
-            vis_pcd.poll_events()
-            vis_pcd.update_renderer()
-            vis_volume.poll_events()
-            vis_volume.update_renderer()
+            # vis_pcd.poll_events()
+            # vis_pcd.update_renderer()
+            # vis_volume.poll_events()
+            # vis_volume.update_renderer()
             i += 1
-        vis_pcd.destroy_window()
-        vis_volume.destroy_window()
+            # while(True):
+            #     pass
+        # vis_pcd.destroy_window()
+        # vis_volume.destroy_window()
 
 
         

transforms.py
@@ -25,10 +25,21 @@ class Transforms():
         #     torch.from_numpy(depthMap[:, :2]), cameraSpace).long()
         # cameraSpace = torch.matmul(
         #     intermediate[:, :3], np.linalg.inv(CamDetails.depthIntrinsics).T)
-        for i in range(CamDetails.depthHeight):
-            for j in range(CamDetails.depthWidth):
-                x = (j - CamDetails.cX) / CamDetails.fX
-                y = (i - CamDetails.cY) / CamDetails.fY
+        X_range = range(depthMap.shape[0])
+        Y_range = range(depthMap.shape[1])
+        
+        # X,Y  = np.meshgrid(range(depthMap.shape[0]), range(depthMap.shape[1]))
+        # print(X.shape, Y.shape)
+        # Z = (depthMap).reshape(-1, 1)
+        # X = (X.reshape(-1, 1) - CamDetails.cX) * Z / CamDetails.fX
+        # Y = (Y.reshape(-1, 1) - CamDetails.cY) * Z / CamDetails.fY
+        # cameraSpace = np.hstack([X, Y, Z]).reshape(depthMap.shape[0], depthMap.shape[1], 3)
+        
+        
+        for i in range(depthMap.shape[0]):
+            for j in range(depthMap.shape[1]):
+                x = (i - CamDetails.cX) / CamDetails.fX
+                y = (j - CamDetails.cY) / CamDetails.fY
                 depthAtPixel = depthMap[i,j]
                 if(depthAtPixel != -math.inf):
                     cameraSpace[i,j] = np.array([x*depthAtPixel, y*depthAtPixel, depthAtPixel])